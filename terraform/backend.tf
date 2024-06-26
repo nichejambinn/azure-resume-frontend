c9fccafb7c5769f789c89bf15b1f7c84a1891f1d
nichejambinn <nichejambinn@gmail.com>
2024-06-26 19:29:30 -0400

    Formatted backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = var.state_resource_group_name
+    resource_group_name  = var.state_resource_group_name
     storage_account_name = var.state_storage_account_name
-    container_name = "tfstate"
-    key = "terraform-base.tfstate"
+    container_name       = "tfstate"
+    key                  = "terraform-base.tfstate"
   }
 }
 
